fbe57c91eb6566648cd6d87735963988375e1c25
nav643 <mashood.yunus@gmail.com>
2017-06-23 18:03:56 -0500

    restructure
    and again

--- UdemyCompleteJavaCourse/src/HelloWorld.java
+++ UdemyCompleteJavaCourse/src/HelloWorld.java
@@ -1,9 +1,9 @@
 
-public class testTraining {
+public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Test traijning");
+		System.out.println("Hello, World!");
 	}
 
 }
